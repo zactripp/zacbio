907a46112e66431341ab2a145ebae014d1790454
Matt <director@acidgambit.com>
2024-12-13 20:50:54 -0500

    bold card

--- src/components/mono-card.tsx
+++ src/components/mono-card.tsx
@@ -8,7 +8,7 @@ interface MonoCardProps {
 export default function MonoCard({ title, children }: MonoCardProps) {
   return (
     <div className="w-full max-w-3xl border border-neutral-950 font-mono">
-      <div className="border-b border-neutral-950 px-4 py-2 text-center text-sm uppercase">
+      <div className="font-bold border-b border-neutral-950 px-4 py-2 text-center text-sm uppercase">
         {title}
       </div>
       <div className="px-4 py-2">
