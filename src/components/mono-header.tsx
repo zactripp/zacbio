f91c0d9997b07ff0f56800918b6f11f49698f122
Matt <director@acidgambit.com>
2024-12-13 20:51:33 -0500

    upd

--- src/components/mono-header.tsx
+++ src/components/mono-header.tsx
@@ -7,7 +7,7 @@ interface MonoHeaderProps {
 export default function MonoHeader({ title }: MonoHeaderProps) {
   return (
     <div className="w-full max-w-3xl border border-neutral-950 font-mono">
-      <div className="border-neutral-950 px-4 py-2 text-center text-sm uppercase">
+      <div className="font-bold border-neutral-950 px-4 py-2 text-center text-sm uppercase">
         {title}
       </div>
     </div>
